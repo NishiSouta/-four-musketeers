21ee2af5f944428128646bc4fc7732fc6e79a1da
yusei888 <2201146@s.asojuku.ac.jp>
2024-10-22 10:06:12 +0900

    Create db-connect.php

---
+++ システム開発四銃士/db-connect.php
@@ -0,0 +1,9 @@
+<?php
+const SERVER ='mysql309.phy.lolipop.lan';
+const DBNAME ='LAA1516804-sports';
+const USER ='LAA1516823';
+const PASS ='pass0905';
+
+$connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+
+?>
