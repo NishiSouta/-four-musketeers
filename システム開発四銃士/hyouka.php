36df6edf8193eb9aca0a53380b3de849c718054d
eto-taisei <148730594+eto-taisei@users.noreply.github.com>
2024-11-14 11:16:58 +0900

    Update hyouka.php

--- システム開発四銃士/hyouka.php
+++ システム開発四銃士/hyouka.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>ユーザーリスト画面</title>
   <link rel="stylesheet" href="css/hyouka.css">
+  <script src="js/openclose.js"></script>
 </head>
 <body>
   <?php
@@ -20,11 +21,30 @@
     <!-- アイコンとタイトル部分 -->
     <header>
     <h1 id="logo"><a href="index.html"><img src="images/LS.png" alt="Photo Gallery"></a></h1>
-    <aside id="header-img"><div id="close-btn"></div></aside>
     </header>
+    <nav id="menubar">
+	<ul>
+	<li><a href="index.html">ホーム</a></li>
+	<li class="current"><a href="about.html">プロフィール</a></li>
+	<li><a href="gallery.html">投稿一覧</a></li>
+	<li><a href="link.php">募集する</a></li>
+	<li><a href="contact.html">ログアウト</a></li>
+	</ul>
+	</nav>
+	
+	<!--小さな端末用（900px以下端末）メニュー-->
+	<nav id="menubar-s">
+	<ul>
+	<li><a href="index.html">ホーム</a></li>
+	<li><a href="about.html">プロフィール</a></li>
+	<li><a href="gallery.html">投稿一覧</a></li>
+	<li><a href="link.php">募集する</a></li>
+	<li><a href="contact.html">ログアウト</a></li>
+	</ul>
+	</nav>
 
     <!-- メッセージ -->
-    <h1>このユーザーが良いと思ったらgoodボタンをお願いします</h1>>
+    <h1>このユーザーが良いと思ったらgoodボタンをお願いします</h1>
 
     <!-- ユーザーリスト -->
     <div class="user-list">
@@ -39,6 +59,15 @@
 
     <!-- TOPページへボタン -->
     <button class="top-button">TOPページへ</button>
+    <!--メニュー開閉ボタン-->
+<div id="menubar_hdr" class="close"></div>
+
+<!--メニューの開閉処理条件設定　900px以下-->
+<script>
+if (OCwindowWidth() <= 900) {
+	open_close("menubar_hdr", "menubar-s");
+}
+</script>
   </div>
 </body>
 </html>
