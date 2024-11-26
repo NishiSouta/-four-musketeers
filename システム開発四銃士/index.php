c689aa55dd669cd1b8fc6bf2c34580a60afbef96
yusei888 <2201146@s.asojuku.ac.jp>
2024-11-26 10:59:29 +0900

    作成

--- システム開発四銃士/index.php
+++ システム開発四銃士/index.php
@@ -5,7 +5,7 @@ require 'db-connect.php'; ?>
 <head>
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>フォトギャラリーサイト向け 無料ホームページテンプレート tp_photo5</title>
+<title>ホーム画面</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="ここにサイト説明を入れます">
 <meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５">
